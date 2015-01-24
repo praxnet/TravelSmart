41a632d860c2ebaa1c5a1c52046f48dfc2a32638
ShekharKG <shekhar114@gmail.com>
2015-01-25 00:45:03 +0530

    Progress dialog is added

--- app/src/main/java/travelsmart/shekharkg/com/travelsmart/GetSearchResult.java
+++ app/src/main/java/travelsmart/shekharkg/com/travelsmart/GetSearchResult.java
@@ -1,5 +1,7 @@
 package travelsmart.shekharkg.com.travelsmart;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.util.Log;
 
@@ -9,20 +11,30 @@ import android.util.Log;
 public class GetSearchResult extends AsyncTask<String, Void, String> {
 
   private MainActivity mainActivity;
+  private Dialog dialog;
 
   public GetSearchResult(MainActivity mainActivity) {
     this.mainActivity = mainActivity;
   }
 
   @Override
+  protected void onPreExecute() {
+    super.onPreExecute();
+    dialog = new ProgressDialog(mainActivity);
+    dialog.setTitle("Loading...");
+    dialog.show();
+  }
+
+  @Override
   protected String doInBackground(String... params) {
-    Log.d("Url is",params[0]);
+    Log.d("Url is", params[0]);
     return GetJsonString.GET(params[0]);
   }
 
   @Override
   protected void onPostExecute(String result) {
     mainActivity.searchOutput(result);
+    dialog.dismiss();
   }
 
 }
